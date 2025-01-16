6c9e007584f21b103aa382149d8699edea0270b6
User <jahidhasan1893>
2025-01-16 07:34:03 +0600

    refactored blog skeleton

--- frontend/app/blogs/page.tsx
+++ frontend/app/blogs/page.tsx
@@ -1,10 +1,41 @@
 import BlogsSkeleton from "@/components/BlogsSkeleton";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
 
 export default function AllBlogsPage(){
     return (
         <div className="flex flex-col w-full">
             <h1 className="flex justify-center mt-10 font-semibold text-xl"> All Blogs Page </h1>
             <BlogsSkeleton />
+             <Pagination className="mb-5">
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">2</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">3</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </div>
     )
 }
--- frontend/components/BlogsSkeleton.tsx
+++ frontend/components/BlogsSkeleton.tsx
@@ -1,42 +1,14 @@
 import BlogsRow from "@/components/BlogsRow";
-import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-} from "@/components/ui/pagination"
+import BlogCard from "./blogcard";
 
 
 export default function BlogsSkeleton(){
     return (
-        <div className="mt-10">
-            <BlogsRow />
-            <BlogsRow />
-            <Pagination className="mb-5">
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious href="#" />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink href="#">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink href="#">2</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink href="#">3</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext href="#" />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+        <div className="mt-10 mb-10 grid grid-cols-3 gap-5 justify-center mx-auto max-w-6xl">
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />   
+            <BlogCard />
 
         </div>
     )
